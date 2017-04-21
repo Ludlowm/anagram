950c696d099002c766de37a7aa654cfdde49d44c
Marko Ludlow <ludlow.marko@gmail.com>
2017-04-21 13:23:27 -0700

    add regex to find if there are vowels in the input.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,18 @@
 class String
-  define_method(:anagram) do
+  def anagram
+    vowel_word = self.downcase
     split_word = self.downcase.split(" ").to_a
     split_letters = self.downcase.split("").to_a
     word1 = split_word[0]
     word2 = split_word[1]
-    if split_letters == split_letters.reverse
-      "These words are palindromes"
-    elsif word1.chars.sort == word2.chars.sort
+      if vowel_word !~ /[aeiouy]/
+        "Please enter in an actual word"
+      elsif split_letters == split_letters.reverse
+        "These words are palindromes"
+      elsif word1.chars.sort == word2.chars.sort
       "These words are anagrams"
-    else
-      "You don't have an anagram"
+      else
+        "You don't have an anagram"
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,13 +3,16 @@ require('anagram')
 require('pry')
 
 describe('String#anagram') do
-  it("returns true if the two words entered are an anagram") do
+  it("returns if the two words entered are an anagram") do
     expect("ruby bury".anagram()).to(eq("These words are anagrams"))
   end
-  it("returns true even if the two words entered have capitalization") do
+  it("returns even if the two words entered have capitalization") do
     expect("Ruby Bury".anagram()).to(eq("These words are anagrams"))
   end
-  it("returns true if the two words entered is an anagram and a palindrome") do
+  it("returns if the two words entered are an anagram and a palindrome") do
     expect("dog god".anagram()).to(eq("These words are palindromes"))
+  end
+  it("returns if the word entered is an actual sentence") do
+    expect("zzzzzzz".anagram()).to(eq("Please enter in an actual word"))
   end
 end
