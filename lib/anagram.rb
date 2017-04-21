cd5ccaaebc93af770f65314635d89be278927a44
Marko Ludlow <ludlow.marko@gmail.com>
2017-04-21 10:26:30 -0700

    add palindrome algorithm to check if word is same as the reverse.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,15 @@
 class String
   define_method(:anagram) do
-    split_word = self.split(" ").to_a
-    word1=split_word[0]
+    split_word = self.downcase.split(" ").to_a
+    split_letters = self.downcase.split("").to_a
+    word1 = split_word[0]
     word2 = split_word[1]
-    if word1.chars.sort == word2.chars.sort
-      "these words are anagrams"
+    if split_letters == split_letters.reverse
+      "These words are palindromes"
+    elsif word1.chars.sort == word2.chars.sort
+      "These words are anagrams"
+    else
+      "You don't have an anagram"
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,9 +4,12 @@ require('pry')
 
 describe('String#anagram') do
   it("returns true if the two words entered are an anagram") do
-    expect("dog god".anagram()).to(eq("these words are anagrams"))
+    expect("ruby bury".anagram()).to(eq("These words are anagrams"))
   end
   it("returns true even if the two words entered have capitalization") do
-    expect("Dog God".anagram()).to(eq("these words are anagrams"))
+    expect("Ruby Bury".anagram()).to(eq("These words are anagrams"))
+  end
+  it("returns true if the two words entered is an anagram and a palindrome") do
+    expect("dog god".anagram()).to(eq("These words are palindromes"))
   end
 end
