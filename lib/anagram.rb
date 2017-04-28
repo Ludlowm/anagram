b2bf3f05a337cc3598fa861cc54a238286feaf54
Marko Ludlow <ludlow.marko@gmail.com>
2017-04-27 21:56:00 -0700

    add new variable to allow multiple words to work and an if statement to show if words arent any of the above.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,24 +1,27 @@
 class String
   def anagram?(input)
     vowel_word = self.downcase
+    input_word = input.downcase
     letters_array = self.gsub(/[^a-zA-Z]/, '').downcase.split("")
     second_array = input.gsub(/[^a-zA-Z]/, '').downcase.split("")
     word1 = letters_array.sort
     word2 = second_array.sort
     empty_array = []
-      if vowel_word !~ /[aeiouy]/
-        "Please enter in an actual word"
-      elsif ((letters_array) == (letters_array)) && ((letters_array) == (second_array.reverse))
+      if  vowel_word !~ /[aeiouy]/ || input_word !~ /[aeiouy]/
+      "Please enter in an actual word"
+      elsif ((letters_array) == (letters_array)) && ((letters_array) ==       (second_array.reverse))
         "These words are palindromes"
       elsif word1 == word2
         "These words are anagrams"
       elsif
-        input.split('').each do |letter|
-          empty_array.push(self.include?(letter))
-
+      input.split('').each do |letter|
+      empty_array.push(self.include?(letter))
       end
-      !(empty_array.include?(true))
+      if !(empty_array.include?(true))
         "This is an antigram"
+      else
+        "This is not an antigram/anagram or palindrome"
+      end
     end
   end
 end
